26158d66ede21bc18d1b61a16f48ae1683d58569
David Eriksson <david@2good.nu>
2017-07-26 12:01:34 +0200

    First parameter to assertEquals should be expected value and second actual value

--- test/test.php
+++ test/test.php
@@ -7,38 +7,38 @@ class FiltersTest extends PHPUnit_Framework_TestCase
 
   public function testZoneFilter()
   {
-  
+
     $this->assertEquals(
-      turfmarkup_zone_filter('zone:Hillbilly'),
-      '<a href="https://turfgame.com/map/Hillbilly" target="_blank">Hillbilly</a>'
+      '<a href="https://turfgame.com/map/Hillbilly" target="_blank">Hillbilly</a>',
+	  turfmarkup_zone_filter('zone:Hillbilly')
     );
 
     $this->assertEquals(
-      turfmarkup_zone_filter('.zone:Hultagölen,'),
-      '.<a href="https://turfgame.com/map/Hultag'.urlencode('ö').'len" target="_blank">Hultag&ouml;len</a>,'
+      '.<a href="https://turfgame.com/map/Hultag'.urlencode('ö').'len" target="_blank">Hultag&ouml;len</a>,',
+	  turfmarkup_zone_filter('.zone:Hultagölen,')
     );
 
     $this->assertEquals(
-      turfmarkup_zone_filter('abc zone:åäö efg'),
-      'abc <a href="https://turfgame.com/map/'.urlencode('åäö').'" target="_blank">&aring;&auml;&ouml;</a> efg'
+      'abc <a href="https://turfgame.com/map/'.urlencode('åäö').'" target="_blank">&aring;&auml;&ouml;</a> efg',
+	  turfmarkup_zone_filter('abc zone:åäö efg')
     );
 
   }
 
   public function testPlayerFilter()
   {
-  
+
     $this->assertEquals(
-      turfmarkup_player_filter('player:TurfBlekingeIT'),
-      '<a href="https://turfgame.com/user/TurfBlekingeIT" target="_blank">TurfBlekingeIT</a>'
+	  '<a href="https://turfgame.com/user/TurfBlekingeIT" target="_blank">TurfBlekingeIT</a>',
+      turfmarkup_player_filter('player:TurfBlekingeIT')
     );
 
     $this->assertEquals(
-      turfmarkup_player_filter('player:fårahedern'),
-      '<a href="https://turfgame.com/user/farahedern" target="_blank">f&aring;rahedern</a>'
+      '<a href="http://turfgame.com/user/farahedern" target="_blank">f&aring;rahedern</a>',
+	  turfmarkup_player_filter('player:fårahedern')
     );
 
-    $this->assertEquals(setlocale(LC_ALL, 'C'), 'C');
+    $this->assertEquals('C', setlocale(LC_ALL, 'C'));
 
   }
 
